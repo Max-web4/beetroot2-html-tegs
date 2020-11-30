53f13a7351356338cd1dcfa450bc031cb7e69205
Max <yudinmax4444@gmail.com>
2020-11-30 17:56:47 +0200

    some changes

---
+++ js_Start/js/main.js
@@ -0,0 +1,12 @@
+let userName = prompt(`введите пвроль`, "") ;
+
+
+if (userName == `Admin` )
+alert(`sing in`), prompt(`sing password`,"")
+if(`sign password`= `I'm boss`) 
+alert(`hi boss`);
+else if (userName==``) 
+alert(`Denied`);
+else(`I don't know you`);
+
+alert(userName)
